149316bb03c5ceffba2e172400d10f7509b4a4b8
Michael Phiri <michael.codesjs@gmail.com>
2022-08-28 20:21:43 +0200

    fix: forgot popular-anime skeletons

--- src/features/anime/popular-anime/index.tsx
+++ src/features/anime/popular-anime/index.tsx
@@ -61,7 +61,7 @@ export function PopularAnime() {
 
           <div className={"flex flex-row md:flex-col md:h-[240px] md:w-full md:overflow-scroll md:pb-8 space-x-6 md:space-x-0 md:space-y-5 p-[1px]"}>
             {memoizedContent}
-            
+            {memoizedSkeletons}
           </div>
 
           <SeeMoreButton />
